c1e6ddb26b8d9f025749140e930b0cb313df3c98
SEHKK0 <javidm1999@gmail.com>
2019-05-10 00:07:34 +0200

    arreglado el crono

--- intervalTimer/src/intervaltimer/FXMLChronoController.java
+++ intervalTimer/src/intervaltimer/FXMLChronoController.java
@@ -48,9 +48,8 @@ public class FXMLChronoController implements Initializable,Runnable,ActionListen
             //Mientras cronometroActivo sea verdadero entonces seguira
             //aumentando el tiempo
             while (cronometroActivo) {
-                Thread.sleep(4);
-                //Incrementamos 4 milesimas de segundo
-                milesimas += 4;
+                Thread.sleep(100);
+                milesimas += 100;
 
                 //Cuando llega a 1000 osea 1 segundo aumenta 1 segundo
                 //y las milesimas de segundo de nuevo a 0
